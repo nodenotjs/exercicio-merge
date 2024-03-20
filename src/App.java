54bbe260c0389b82350af96c26bb3c4d346bf56f
nodenotjs <diogodogogo@gmail.com>
2024-03-20 20:56:43 -0300

    Login page added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Usuário: blablabla");
+        System.out.println("Senha: ****");
     }
 }
