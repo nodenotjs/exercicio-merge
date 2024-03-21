18aede80b282003f73c255769e7100cf10466ed6
nodenotjs <diogodogogo@gmail.com>
2024-03-20 21:11:36 -0300

    fixed text

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Usuário: blablabla");
+        System.out.println("Usuário cadastro: blablabla");
         System.out.println("Senha: ****");
     }
 }
