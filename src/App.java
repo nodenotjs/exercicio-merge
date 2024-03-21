cc8b8470ab28387b1b944a7f869e2a32296f65cd
nodenotjs <diogodogogo@gmail.com>
2024-03-20 21:24:03 -0300

    done

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Usuário cadastro: blablabla");
-=======
         System.out.println("Usuário login: blablabla");
->>>>>>> main
         System.out.println("Senha: ****");
     }
 }
